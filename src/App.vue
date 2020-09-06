7a6e4d477bd85ea712117531f7fc18c0bc0a6f99
Udit Namdev <unamdev0@gmail.com>
2020-09-06 17:31:32 +0530

    Quiz part almost done,charts and visualization remaining

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,68 @@
       <div class="quiz-header">
         <h1>Welcome</h1>
       </div>
-      <div class="main-quiz" v-for="index in 10" :key="index">
+      <div class="main-quiz" v-show="!show_marks" >
         <div class="question-box">
-            <h1>question</h1>
-            <!-- <p>{{element.question}}</p> -->
+          <h1>question</h1>
+          <p>{{questions[questionIndex].question}}</p>
         </div>
         <div class="options">
           <ul>
-            <li>
-
-            </li>
+            <li
+              v-for="ans in questions[questionIndex].answers"
+              :key="ans"
+              @click="selectComponent(ans,$event)"
+            >{{ans}}</li>
           </ul>
-          
         </div>
       </div>
+      <div class="main-quiz" v-show="show_marks">
+        <h1>You Scored:{{marks}}/10</h1>
+      </div>
 
-      <div class="footer-quiz"></div>
+      <div class="footer-quiz">
+        <button v-if="!show_marks" @click="finishQuiz()">Finish</button>
+        <button v-if="!show_marks" @click="nextQuestion()">Next</button>
+        <button v-if="show_marks" @click="startQuiz()">Restart</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import json from './assets/data.json'
+import json from "./assets/data.json";
 export default {
   name: "App",
-  data(){
-    return{
-      questions:json.questions
-    }
-  }
+  data() {
+    return {
+      questions: json.questions,
+      questionIndex: Math.floor(Math.random() * 15),
+      selected: false,
+      marks: 0,
+      show_marks:false,
+      finished:false,
+    };
+  },
+  methods: {
+    selectComponent(component, event) {
+      if (this.selected) {
+        this.selected.classList.remove("select");
+      }
+      this.selected = event.target;
+      this.selected.classList.add("select");
+      if (component.correct) {
+        this.marks += 1;
+      }
+    },
+    nextQuestion() {
+      (this.questionIndex = Math.floor(Math.random() * 15)),
+      this.selected = null;
+    },
+     finishQuiz() {
+      this.show_marks=true
+      this.selected = null;
+    },
+  },
 };
 </script>
 
