ef4f63bb85d601f023db96b126675ae1e1fcddcc
Udit Namdev <unamdev0@gmail.com>
2020-09-07 18:29:29 +0530

    Basic idea of stats->done

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,85 @@
 <template>
-  <div class="container">
-    <div class="container-quiz">
-      <div class="quiz-header">
-        <h1>Welcome</h1>
-      </div>
-      <div class="main-quiz" v-show="!show_marks" >
-        <div class="question-box">
-          <h1>question</h1>
-          <p>{{questions[questionIndex].question}}</p>
-        </div>
-        <div class="options">
-          <ul>
-            <li
-              v-for="ans in questions[questionIndex].answers"
-              :key="ans"
-              @click="selectComponent(ans,$event)"
-            >{{ans}}</li>
-          </ul>
-        </div>
-      </div>
-      <div class="main-quiz" v-show="show_marks">
-        <h1>You Scored:{{marks}}/10</h1>
-      </div>
-
-      <div class="footer-quiz">
-        <button v-if="!show_marks" @click="finishQuiz()">Finish</button>
-        <button v-if="!show_marks" @click="nextQuestion()">Next</button>
-        <button v-if="show_marks" @click="startQuiz()">Restart</button>
-      </div>
-    </div>
+  <div>
+    <Stats v-bind:chartdata="chart" v-bind:options="options" />
   </div>
 </template>
 
+
 <script>
-import json from "./assets/data.json";
+// import Quiz from './components/Quiz'
+import Stats from "./components/Stats";
 export default {
   name: "App",
+  components: {
+    // Quiz
+    Stats,
+  },
   data() {
     return {
-      questions: json.questions,
-      questionIndex: Math.floor(Math.random() * 15),
-      selected: false,
-      marks: 0,
-      show_marks:false,
-      finished:false,
+      chart: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 12, 3],
+
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+         display: true,
+         scaleLabel: {
+           display: true,
+           labelString: 'Month'
+         }
+       }],
+          yAxes: [{
+         display: true,
+         scaleLabel: {
+           display: true,
+           labelString: 'Value'
+         },
+         ticks: {
+           min: 0,
+           max: 20,
+           stepSize: 1,
+           callback: function(label) {
+             switch (label) {
+               case 0:
+                 return 'ZERO';
+               case 1:
+                 return 'ONE';
+               case 2:
+                 return 'TWO';
+               case 3:
+                 return 'THREE';
+               case 4:
+                 return 'FOUR';
+               case 5:
+                 return 'FIVE';
+               default:
+                 return "fd"
+             }
+           }
+         },
+         gridLines: {
+           display: false
+         }
+       }]
+        },
+      },
+      
     };
-  },
-  methods: {
-    selectComponent(component, event) {
-      if (this.selected) {
-        this.selected.classList.remove("select");
-      }
-      this.selected = event.target;
-      this.selected.classList.add("select");
-      if (component.correct) {
-        this.marks += 1;
-      }
-    },
-    nextQuestion() {
-      (this.questionIndex = Math.floor(Math.random() * 15)),
-      this.selected = null;
-    },
-     finishQuiz() {
-      this.show_marks=true
-      this.selected = null;
-    },
   },
 };
 </script>
 
+<style scoped>
+div{
+  width:50%;
+  height: 50%;
+}
+</style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <div class="container-quiz">
+      <div class="quiz-header">
+        <h1>Welcome</h1>
+      </div>
+      <div class="main-quiz" v-show="!show_marks">
+        <div class="question-box">
+          <h1>{{questions[questionIndex].question}}</h1>
+        </div>
+        <div class="options">
+          <ul>
+            <li
+              v-for="ans in questions[questionIndex].answers"
+              :key="ans"
+              @click="selectComponent(ans,$event)"
+            >{{ans.option}}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="main-quiz" v-show="show_marks">
+        <h1>You Scored:{{marks}}/10</h1>
+      </div>
+
+      <div class="footer-quiz">
+        <button v-if="!show_marks" @click="finishQuiz()">Finish</button>
+        <button v-if="!show_marks" @click="nextQuestion()">Next</button>
+        <button v-if="show_marks" @click="startQuiz()">Restart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import json from "../assets/data.json";
+export default {
+  name: "Quiz",
+  data() {
+    return {
+      questions: json.questions,
+      questionIndex: Math.floor(Math.random() * 15),
+      component: null,
+      selected: false,
+      marks: 0,
+      show_marks: false,
+      counter: 0,
+    };
+  },
+  methods: {
+    selectComponent(component, event) {
+      if (this.selected) {
+        this.selected.classList.remove("select");
+      }
+      this.selected = event.target;
+      this.selected.classList.add("select");
+      this.component = component;
+    },
+    nextQuestion() {
+      if (this.counter >= 10) {
+        this.finishQuiz();
+      }
+      (this.questionIndex = Math.floor(Math.random() * 19)),
+        (this.counter += 1);
+      if (this.component != null && this.component.correct) {
+        this.marks += 1;
+      }
+    },
+    finishQuiz() {
+      this.show_marks = true;
+      if (this.component != null && this.component.correct) {
+        this.marks += 1;
+      }
+      this.selected = null;
+    },
+    startQuiz() {
+      this.show_marks = false;
+      this.marks = 0;
+      this.counter = 0;
+      this.selected = null;
+      this.component = null;
+      this.nextQuestion();
+    },
+  },
+};
+</script>
+
